64be358b78af38bc9f3f3ea887e92fb574152745
John Doe <john.doe@example.com>
2024-12-31 10:36:47 +0900

    업로드

---
+++ src/main/resources/static/user/userHome/js/loadMainHover.js
@@ -0,0 +1,7 @@
+fetch('mainHover.html')
+    .then((response) => response.text())
+    .then((data) => {
+        // HTML 내용을 body의 맨 위에 삽입
+        document.body.insertAdjacentHTML('afterbegin', data);
+    })
+    .catch((error) => console.error('Error loading HTML:', error));
