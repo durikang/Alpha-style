acc1e3924b33843e902fd13455102ab426396272
John Doe <john.doe@example.com>
2024-12-31 16:30:02 +0900

    오늘 userhome부분을 넣고 배너쪽 사진과 상품사진 배치하는 작업을 했습니다

--- src/main/resources/static/user/userHome/js/usermainfooter.js
+++ src/main/resources/static/user/userHome/js/usermainfooter.js
@@ -1,4 +1,4 @@
-fetch('../usermainfooter.html')
+fetch('../../templates/user/usermainfooter.html')
     .then((response) => response.text())
     .then((data) => {
         // HTML 내용을 body의 맨 아래에 삽입
